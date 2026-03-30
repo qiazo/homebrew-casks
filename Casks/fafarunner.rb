92477516bce84c3a5155b6faf2ca7d4d998e67bf
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2026-03-30 17:15:19 +0000

    Update fafarunner cask to v2.8.0
    Signed-off-by: kjxbyz <47768002+kjxbyz@users.noreply.github.com>

--- Casks/fafarunner.rb
+++ Casks/fafarunner.rb
@@ -1,6 +1,6 @@
 cask "fafarunner" do
-  version "2.7.0"
-  sha256 "fcd015f60b85e03587fbc62d0d791b8a4db588300cfc1c0433226a6797a742fe"
+  version "2.8.0"
+  sha256 "e91650a24f4e5dd8246c1da4aace4ea32b4de205cebf2e01aa74a38ecaee5af6"
 
   url "https://github.com/fafarunner/fafarunner/releases/download/v#{version}/FaFaRunner-#{version}-macos-universal.dmg",
       verified: "github.com/fafarunner/fafarunner/"
